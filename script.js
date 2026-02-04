cd385499bf7cab10593db719fe2ccecffa2ade85
Katlego Twala <212864722+kattdevs@users.noreply.github.com>
2026-02-04 12:42:16 +0200

    Incorporate literature questions into bookQuiz array
    Added a series of literature-related quiz questions to enhance the book quiz functionality.

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ class Question {
       return choice === this.answer;
     }
 }
+//BOOKS (LITERATURE) QUESTIONS
+const bookQuiz = [
+    new Question(
+        "Who wrote 'Romeo and Juliet'?",
+        ["Willliam Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"],
+        "William Shakespeare"
+    ),
+    new Question(
+        "Which novel starts with 'Call me Ishmael'?",
+        ["Mobby-Dick", "Dracula", "Frankenstein", "The Great Gatsby"],
+        "Moby-Dick"
+    ),
+    new Question(
+        "Who is the author of 'Pride and Prejudice'?",
+        ["Jane Austen", "Emily Brontë", "Virginia Woolf", "George Eliot"],
+        "Jane Austen"
+    ),
+    new Question(
+        "Which book features a wizard named Gandalf?",
+        ["The Hobbit", "Harry Potter", "Percy Jackson", "Eragaon"],
+        "The Hobbit"
+    ),
+    new Question(
+        "What is the name of the school in Harry Potter?",
+        ["Hogwarts", "Beauxbatons", "Durmstrang", "Ilvermorny"],
+        "Hogwarts"
+    )
+];
