7d4428d5e720d383bf06faa09b1d80bf857aec44
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-04 15:19:47 +0200

    Remove duplicate start screen functionality and related event listeners

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// START SCREEN
-const startBtn = document.getElementById('startBtn');
-const startScreen = document.getElementById('start');
-const homeScreen = document.getElementById('home');
-
-startBtn.addEventListener('click', () => {
-  startScreen.classList.remove('active');
-  homeScreen.classList.add('active')
-})
-
-
 class Question {
     constructor(text, choices, answer) {
       this.text = text;
